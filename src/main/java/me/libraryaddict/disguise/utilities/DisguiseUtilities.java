fb5ec972129fd7c38dcb6239eba61479652528c9
libraryaddict <github@lib.co.nz>
2020-01-19 15:48:52 +1300

    Add setNameVisible to player disguises

--- src/main/java/me/libraryaddict/disguise/utilities/DisguiseUtilities.java
+++ src/main/java/me/libraryaddict/disguise/utilities/DisguiseUtilities.java
@@ -911,7 +911,7 @@ public class DisguiseUtilities {
         return gson;
     }
 
-    public static void init(LibsDisguises disguises) {
+    public static void init() {
         try {
             runningPaper = Class.forName("com.destroystokyo.paper.VersionHistoryManager$VersionData") != null;
         }
@@ -1034,6 +1034,12 @@ public class DisguiseUtilities {
             catch (Exception ex) {
                 getLogger().warning("The file '" + key + "' does not belong in " + savedDisguises.getAbsolutePath());
             }
+        }
+
+        registerNoName(Bukkit.getScoreboardManager().getMainScoreboard());
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            registerNoName(player.getScoreboard());
         }
     }
 
@@ -1337,6 +1343,18 @@ public class DisguiseUtilities {
         }
 
         player.updateInventory();
+    }
+
+    public static void registerNoName(Scoreboard scoreboard) {
+        Team mainTeam = scoreboard.getTeam("LD_NoName");
+
+        if (mainTeam == null) {
+            mainTeam = scoreboard.registerNewTeam("LD_NoName");
+            mainTeam.setOption(Option.NAME_TAG_VISIBILITY, OptionStatus.NEVER);
+            mainTeam.addEntry("");
+        } else if (!mainTeam.hasEntry("")) {
+            mainTeam.addEntry("");
+        }
     }
 
     public static void removeSelfDisguiseScoreboard(Player player) {
